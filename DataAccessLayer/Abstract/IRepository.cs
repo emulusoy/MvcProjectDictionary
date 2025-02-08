d700a653d57e338fc71c0d8ae31eef9b5ff941a0
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-02-08 19:20:25 +0300

    .

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T> //bu t ben hangi repository gonderirsem o olacak!
+    {
+        List<T> List();
+        void Insert(T p);
+        void Delete(T p);
+        void Update(T p);
+        List<T> List(Expression<Func<T, bool>>filter);//burasi da sartli listeleme yazar adi su olani getir gibi
+    }
+}
