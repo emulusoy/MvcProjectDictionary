4890e428019d3027c3e8453d3b252d7a5fd90564
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-02-09 22:32:24 +0300

    .

---
+++ BusinessLayer/ValidationRules_FluentValidation/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules_FluentValidation
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Required field to be filled");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Required field to be filled");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Required field to be filled");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Required field to be filled");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Required field to be filled");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Required field to be filled");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("En az 2 karakter olmali!");
+            RuleFor(x => x.WriterSurname).MaximumLength(30).WithMessage("30 Karakterden fazla giris yapamazsiniz!");
+        }
+    }
+}
---
+++ MvcProjectEcommerce/Controllers/WriterController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+
+namespace MvcProjectEcommerce.Controllers
+{
+    public class WriterController : Controller
+    {
+        // GET: Writer
+        WriterManager writerManager=new WriterManager(new EfWriterDal());
+        WriterValidator writerValidation = new WriterValidator();
+        public ActionResult Index()
+        {
+            var writerValues = writerManager.GetList();
+            return View(writerValues);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            
+            ValidationResult results = writerValidation.Validate(p);
+            if (results.IsValid)
+            {
+                writerManager.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UpdateWriter(int id) 
+        { 
+                var writerValue = writerManager.GetById(id);
+            return View(writerValue);
+        }
+        [HttpPost]
+        public ActionResult UpdateWriter(Writer p)
+        {
+            ValidationResult results = writerValidation.Validate(p);
+            if (results.IsValid)
+            {
+                writerManager.WriterUpdate(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();          
+        }
+    }
+}
